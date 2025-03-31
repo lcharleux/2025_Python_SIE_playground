e2365dbe819f9ba568bf4a538a09a6b18835774b
brunel <brunelle@protonmail.com>
2025-03-31 09:28:15 +0200

    update

--- src/demos/recognize_amplitude_frequence.py
+++ src/demos/recognize_amplitude_frequence.py
@@ -8,20 +8,20 @@ import pandas as pd
 def recognize_amplitude_frequence (path_file : str):
     #identifie si le fichier .csv a déjà des headers : si oui, on les garde, si non, on les ajoute
     temp=pd.read_csv(path_file,sep=",", header=None)
-    if temp.iloc[0,0]=="signal":
+    if temp.iloc[0,0]=="t":
         data=pd.read_csv(path_file,sep=",", header=0)
     else:
-        data=pd.read_csv(path_file,sep=",", header=None, names=["signal", "amplitude","frequence","noise"])
+        data=pd.read_csv(path_file,sep=",", header=None, names=["t", "x"])
 
     dim=data.shape
     print(data)
     for i in range(dim[0]):
-        print (f"Signal {data.iloc[i,data.columns.get_loc("signal")]} a une amplitude de {data.iloc[i,data.columns.get_loc("amplitude")]} et une fréquence de {data.iloc[i,data.columns.get_loc("frequence")]} ")
+        print (f"Signal {i} au temps {data.iloc[i,data.columns.get_loc("t")]} a une amplitude de {data.iloc[i,data.columns.get_loc("x")]} ")
 
 
 if __name__ == "__main__":
     #on récupère la liste des fichiers .csv dans un dossier donné
-    liste_fichiers=os.listdir(r"C:\Users\BA251988\Documents\Python\cours_annecy")
+    liste_fichiers=os.listdir(r"..\doc\data_processing\data")
 
     # liste_fichiers_csv=[]
 
